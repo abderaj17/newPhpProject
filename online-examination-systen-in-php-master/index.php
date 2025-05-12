3162bfb80cac0610e2ce1afd34ffeea3e2befd4f
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-05-13 00:01:33 +0530

    com

--- online-examination-systen-in-php-master/index.php
+++ online-examination-systen-in-php-master/index.php
@@ -198,10 +198,10 @@ var b = document.forms["form"]["cpassword"].value;if (a!=b){alert("Passwords mus
 		 <img src="image/CAM00121.jpg" width=100 height=100 alt="Sunny Prakash Tiwari" class="img-rounded">
 		 </div>
 		 <div class="col-md-5">
-		<a href="http://yugeshverma.blogspot.in" style="color:#202020; font-family:'typo' ; font-size:18px" title="Find on Facebook">Yugesh Verma</a>
-		<h4 style="color:#202020; font-family:'typo' ;font-size:16px" class="title1">+91 9165063741</h4>
-		<h4 style="font-family:'typo' ">vermayugesh323@gmail.com</h4>
-		<h4 style="font-family:'typo' ">Chhattishgarh insitute of management & Technology ,bhilai</h4></div></div>
+		<a href="http://yugeshverma.blogspot.in" style="color:#202020; font-family:'typo' ; font-size:18px" title="Find on Facebook">Arpit Maurya</a>
+		<h4 style="color:#202020; font-family:'typo' ;font-size:16px" class="title1">+91 9565620525</h4>
+		<h4 style="font-family:'typo' ">arpitmaurya367@gmail.com</h4>
+		<h4 style="font-family:'typo' ">Feroze Gandhi Institute Of Engineering And Technology, Raebreli</h4></div></div>
 		</p>
       </div>
     
